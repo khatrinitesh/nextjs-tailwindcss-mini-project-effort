05a70f1aeff81b3cf0ea68502be7570bdd50e8f7
niteshkhatri88 <nitesh.k@adsmn.in>
2024-03-10 10:17:16 +0530

    updated

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,14 +1,14 @@
 import Banner from "@/components/banner";
 // import CustomApp from "../repository/animation/balloonpopping/1/page";
-import TailwindCSSApp from "../tailwindcss/scrollsnaptype/1/page";
-import CustomApp from "../repository/menuicon/1/page";
+// import TailwindCSSApp from "../tailwindcss/scrollsnaptype/1/page";
+// import CustomApp from "../repository/menuicon/1/page"
 
 const About = () => {
   return (
     <div className="mainContent clear-both">
       <Banner bannerStyle='bg-[red]' title="About" desc="Do id tempor laboris laboris sint cillum ad fugiat excepteur cupidatat do adipisicing mollit."/>
-      <TailwindCSSApp/>
-      <CustomApp/>
+      {/* <TailwindCSSApp/> */}
+      {/* <CustomApp/> */}
     </div>  
   )
 }
