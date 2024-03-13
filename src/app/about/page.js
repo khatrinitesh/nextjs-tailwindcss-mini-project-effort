e2b97c4fd989d6737eeba062536d5a00b596141b
nitesh <nitesh.khatri88@gmail.com>
2024-03-13 16:51:13 +0530

    updated

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,19 +1,12 @@
 import Banner from "@/components/banner";
-import CustomApp from "../repository/animation/balloonpopping/1/page";
-import TailwindCSSApp from "../tailwindcss/scrollsnaptype/1/page";
+import FetchData from "@/components/restfetchdatafromapi/1/app";
 
 const About = () => {
-  // const items = [
-  //   { title: 'Item 1', desc: 'Content for item 1' },
-  //   { title: 'Item 2', desc: 'Content for item 2' },
-  //   // Add more items as needed
-  // ];
   
   return (
     <div className="mainContent clear-both">
       <Banner bannerStyle='bg-[red]' title="About" desc="Do id tempor laboris laboris sint cillum ad fugiat excepteur cupidatat do adipisicing mollit."/>
-      <TailwindCSSApp/>
-      <CustomApp/>
+      <FetchData/>
     </div>  
   )
 }
